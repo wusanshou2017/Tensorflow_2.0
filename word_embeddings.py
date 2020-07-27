ed8964006c97adcca7b833b675055b7d1daa0bcc
wu_allin <252652905@qq.com>
2020-07-27 18:41:16 +0800

    word_embedding

--- word_embeddings.py
+++ word_embeddings.py
@@ -1,1 +1,9 @@
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import tensorflow_datasets as tfds
+
+embedding_layer = layers.Embedding(1000, 5)
+result = embedding_layer(tf.constant([1, 2, 3]))
+print(result.numpy())

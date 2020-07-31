746212157cf291726e97db35afbde1007167039e
wu_allin <252652905@qq.com>
2020-07-31 16:42:59 +0800

    to implement Transformer

---
+++ Transform_implement_with_tf2.0/input_pipeline.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+import tensorflow_datasets as tfds
+import tensorflow as tf
+
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en', with_info=True,
+                               as_supervised=True)
+train_examples, val_examples = examples['train'], examples['validation']
+print ("finish....")

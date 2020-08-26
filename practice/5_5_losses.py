c1a340aef59522421bea9dceaea619b2a41ba153
wu_allin <252652905@qq.com>
2020-08-26 09:40:09 +0800

    losses

--- practice/5_5_losses.py
+++ practice/5_5_losses.py
@@ -20,3 +20,30 @@ model.compile(optimizer="rmsprop",
               loss="sparse_categorical_crossentropy", metrics=["AUC"])
 
 model.summary()
+
+
+def focal_loss(gamma=2., alpha=.25):
+
+    def focal_loss_fixed(y_true, y_pred):
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+        loss = -tf.sum(alpha * tf.pow(1. - pt_1, gamma) * tf.log(1e-07 + pt_1)) \
+            - tf.sum((1 - alpha) * tf.pow(pt_0, gamma)
+                     * tf.log(1. - pt_0 + 1e-07))
+        return loss
+    return focal_loss_fixed
+
+class FocalLoss(losses.Loss):
+
+    def __init__(self, gamma=2.0, alpha=0.25):
+        self.gamma = gamma
+        self.alpha = alpha
+
+    def call(self, y_true, y_pred):
+
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+        loss = -tf.sum(self.alpha * tf.pow(1. - pt_1, self.gamma) * tf.log(1e-07 + pt_1)) \
+            - tf.sum((1 - self.alpha) * tf.pow(pt_0, self.gamma)
+                     * tf.log(1. - pt_0 + 1e-07))
+        return loss

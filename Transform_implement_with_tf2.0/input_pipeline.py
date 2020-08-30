9e8fd1b4d5080e66848264ba4acca773d931b86e
wu_allin <252652905@qq.com>
2020-08-30 16:24:32 +0800

    some prefix work for transformer

--- Transform_implement_with_tf2.0/input_pipeline.py
+++ Transform_implement_with_tf2.0/input_pipeline.py
@@ -13,5 +13,23 @@ examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en', with_info=True,
 train_examples, val_examples = examples['train'], examples['validation']
 print ("finish....")
 
+
+tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+    (en.numpy() for pt, en in train_examples), target_vocab_size=2**13)
+
+tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+    (pt.numpy() for pt, en in train_examples), target_vocab_size=2**13)
+
+sample_string = 'Transformer is awesome.'
+
+tokenized_string = tokenizer_en.encode(sample_string)
+print ('Tokenized string is {}'.format(tokenized_string))
+
+original_string = tokenizer_en.decode(tokenized_string)
+print ('The original string: {}'.format(original_string))
+
+assert original_string == sample_string
+
+
 def preprocess_text(input_file):
-    pass
+    

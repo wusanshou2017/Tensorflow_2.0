6755dc148f6ba0436a5b785067f112314a43a248
wu_allin <252652905@qq.com>
2020-08-31 18:06:17 +0800

    data_input_pipeline

--- Transform_implement_with_tf2.0/input_pipeline.py
+++ Transform_implement_with_tf2.0/input_pipeline.py
@@ -1,17 +1,73 @@
 # encoding: utf-8
 
-import tensorflow_datasets as tfds
 import tensorflow as tf
 
-import time
-import numpy as np
-import matplotlib.pyplot as plt
+import re
 
+assert(tf.__version__.startswith("2."))
 
-examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en', with_info=True,
-                               as_supervised=True)
-train_examples, val_examples = examples['train'], examples['validation']
-print ("finish....")
 
 def preprocess_text(input_file):
-    pass
+    f = open(input_file, "r", encoding="utf-8")
+    all_lines = f.readlines()
+    eng_lines = [line.split("\t")[0] for line in all_lines]
+    cn_lines = [line.split("\t")[1] for line in all_lines]
+
+    return eng_lines, cn_lines
+
+
+eng_lines, cn_lines = preprocess_text("cmn.txt")
+assert(len(eng_lines) == len(cn_lines))
+
+
+def preprocess_sentence(w):
+
+    w = w.lower().strip()
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
+
+    # 除了 (a-z, A-Z, ".", "?", "!", ",")，将所有字符替换为空格
+    # w = re.sub(r"[a-zA-Z?.!,¿]+", " ", w)
+
+    w = w.rstrip().strip()
+
+    # 给句子加上开始和结束标记
+    # 以便模型知道何时开始和结束预测
+    return w
+
+
+eng_lines = ["<start> " +
+             preprocess_sentence(line) + " <end>" for line in eng_lines]
+cn_lines = ["<start> " + " ".join(line) + " <end>" for line in cn_lines]
+
+print(eng_lines[:10])
+print(cn_lines[:10])
+
+
+def tokenize(lang):
+    lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(
+        filters='')
+    lang_tokenizer.fit_on_texts(lang)
+
+    tensor = lang_tokenizer.texts_to_sequences(lang)
+
+    tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor,
+                                                           padding='post')
+
+    return tensor, lang_tokenizer
+
+
+eng_tensor, eng_tokenizer = tokenize(eng_lines)
+cn_tensor, cn_tokenizer = tokenize(cn_lines)
+print(eng_tensor[:3])
+print(cn_tensor[:3])
+
+
+def load_dataset(path, num_examples=None):
+    # 创建清理过的输入输出对
+    targ_lang, inp_lang = create_dataset(path, num_examples)
+
+    input_tensor, inp_lang_tokenizer = tokenize(inp_lang)
+    target_tensor, targ_lang_tokenizer = tokenize(targ_lang)
+
+    return input_tensor, target_tensor, inp_lang_tokenizer, targ_lang_tokenizer

f5f6356b887e9120d1e17224cf0078423510e70f
wu_allin <252652905@qq.com>
2020-09-14 14:37:02 +0800

    evaluate to be done

---
+++ Transform_implement_with_tf2.0/evaluate_model.py
@@ -0,0 +1,41 @@
+
+input_vocab_size = len(cn_tokenizer.word_index)
+target_vocab_size = len(eng_tokenizer.word_index)
+
+def evaluate(inp_sentence):
+    start_token = [tokenizer_pt.vocab_size]
+    end_token = [tokenizer_pt.vocab_size + 1]
+
+    # 输入语句，增加开始和结束标记
+    inp_sentence = start_token + tokenizer_pt.encode(inp_sentence) + end_token
+    encoder_input = tf.expand_dims(inp_sentence, 0)
+
+    # 因为目标是英语，输入 transformer 的第一个词应该是英语的开始标记。
+    decoder_input = [tokenizer_en.vocab_size]
+    output = tf.expand_dims(decoder_input, 0)
+    
+    for i in range(MAX_LENGTH):
+        enc_padding_mask, combined_mask, dec_padding_mask = create_masks(
+            encoder_input, output)
+  
+        # predictions.shape == (batch_size, seq_len, vocab_size)
+        predictions, attention_weights = transformer(encoder_input, 
+                                                     output,
+                                                     False,
+                                                     enc_padding_mask,
+                                                     combined_mask,
+                                                     dec_padding_mask)
+    
+        # 从 seq_len 维度选择最后一个词
+        predictions = predictions[: ,-1:, :]  # (batch_size, 1, vocab_size)
+
+        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+        
+        # 如果 predicted_id 等于结束标记，就返回结果
+        if predicted_id == tokenizer_en.vocab_size+1:
+            return tf.squeeze(output, axis=0), attention_weights
+    
+        # 连接 predicted_id 与输出，作为解码器的输入传递到解码器。
+        output = tf.concat([output, predicted_id], axis=-1)
+
+    return tf.squeeze(output, axis=0), attention_weights

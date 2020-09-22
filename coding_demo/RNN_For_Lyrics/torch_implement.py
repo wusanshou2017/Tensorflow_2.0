1e174f1555c148ec1808516e6b3c9e6e225d5de2
wu_allin <252652905@qq.com>
2020-09-23 05:36:29 +0800

    add torch

---
+++ coding_demo/RNN_For_Lyrics/torch_implement.py
@@ -0,0 +1,23 @@
+import numpy as np
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+
+import sys
+sys.path.append("..") 
+import d2lzh_pytorch as d2l
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+(corpus_indices, char_to_idx, idx_to_char, vocab_size) = d2l.load_data_jay_lyrics()
+
+
+lr = 1e-2 # 注意调整学习率
+lstm_layer = nn.LSTM(input_size=vocab_size, hidden_size=num_hiddens)
+num_epochs, num_steps, batch_size, lr, clipping_theta = 160, 35, 32, 1e2, 1e-2
+pred_period, pred_len, prefixes = 40, 50, ['想', '不想']
+
+model = d2l.RNNModel(lstm_layer, vocab_size)
+d2l.train_and_predict_rnn_pytorch(model, num_hiddens, vocab_size, device,
+                                corpus_indices, idx_to_char, char_to_idx,
+                                num_epochs, num_steps, lr, clipping_theta,
+                                batch_size, pred_period, pred_len, prefixes)

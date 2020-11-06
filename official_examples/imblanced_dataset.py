f8a498eea0a1b4597fb1ebbd2405d9fb8bf588e2
wu_allin <252652905@qq.com>
2020-11-06 18:28:17 +0800

    solved

---
+++ official_examples/imblanced_dataset.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import os
+import tempfile
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import sklearn
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+mpl.rcParams['figure.figsize'] = (12, 10)
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+file = tf.keras.utils
+raw_df = pd.read_csv('https://storage.googleapis.com/download.tensorflow.org/data/creditcard.csv')
+raw_df.head()
